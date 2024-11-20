dc443f489ff105df5f6e4903809272278e288e2a
Neil Ning <455697473@qq.com>
2024-11-20 22:33:31 +0800

    fix: finish stream render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
+import { hydrate } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {
